18112ed1eef35c026f3522d2ced853565af9cf81
Sun-VanillaSky <18765529231@sina.cn>
2015-03-22 01:03:46 +0800

    空格

--- Node.gd
+++ Node.gd
@@ -1,15 +1,14 @@
-node					#节点
-	CanvasItem			# 画布
-	Spatial				#
-	AnimationPlayer		# 动作编辑器
-	AnimationTreePlayer	# 动作树形编辑器
-	CanvasLayer			# 画布层
-	EditorPlugin		# 编辑器插件
-	EventPlayer			# 事件
-	resourcePreloader	# 资源预载
-	sampleplayer		# 样品播放器
-	SoundRoomParams		# 音室参数
-	streamPlayer		# 流播放器
-	timer				# 定时器
-	tween				# 
-	viewport			# 视窗
+node                    #节点
+    CanvasItem          # 画布
+    Spatial             #
+    AnimationPlayer     # 动作编辑器
+    AnimationTreePlayer # 动作树形编辑器
+    CanvasLayer         # 画布层
+    EditorPlugin        # 编辑器插件
+    EventPlayer         # 事件
+    resourcePreloader   # 资源预载
+    sampleplayer        # 样品播放器
+    SoundRoomParams     # 音室参数
+    timer               # 定时器
+    tween               # 
+    viewport            # 视窗
